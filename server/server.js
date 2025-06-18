a1794293f83044d30f9d0e417e1cbdf33f3c393d
Desalegn Sefiw <166336863+DesuSefiw@users.noreply.github.com>
2025-06-18 13:55:14 +0300

    Update server.js

--- server/server.js
+++ server/server.js
@@ -7,8 +7,11 @@ const trainerRoutes = require('./routes/trainerRoutes');
 const trainers = require('./routes/trainers');
 
 const app = express();
-app.use(cors());
-app.use(express.json());
+app.use(cors({
+  origin: 'https://web-design-training-lilac.vercel.app',
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  credentials: true,
+}));app.use(express.json());
 
 // MongoDB connection
 mongoose.connect(process.env.MONGO_URI, {
