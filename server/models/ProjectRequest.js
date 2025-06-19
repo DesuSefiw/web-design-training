30fc2edbee0b7f9a3192b387c8f6d112058a70b5
Desalegn Sefiw <166336863+DesuSefiw@users.noreply.github.com>
2025-06-19 09:35:40 +0300

    Update ProjectRequest.js

--- server/models/ProjectRequest.js
+++ server/models/ProjectRequest.js
@@ -1,13 +1,15 @@
-// models/ProjectRequest.js
 const mongoose = require('mongoose');
 
-const requestSchema = new mongoose.Schema({
+const ProjectRequestSchema = new mongoose.Schema({
   name: String,
   email: String,
   company: String,
   projectType: String,
   details: String,
-  createdAt: { type: Date, default: Date.now }
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
-module.exports = mongoose.model('ProjectRequest', requestSchema);
+module.exports = mongoose.model('ProjectRequest', ProjectRequestSchema);
