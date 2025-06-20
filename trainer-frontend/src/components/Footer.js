a0d03c5a5081656587644006e415f2ce073707ee
Desalegn Sefiw <166336863+DesuSefiw@users.noreply.github.com>
2025-06-20 10:26:56 +0300

    Update Footer.js

--- trainer-frontend/src/components/Footer.js
+++ trainer-frontend/src/components/Footer.js
@@ -10,14 +10,35 @@ function Footer() {
         <p>© {new Date().getFullYear()} Website Design Training. All rights reserved.</p>
 
         <div className="contact-info">
-          <p><FaPhone /> +251 961787618</p>
-          <p><FaEnvelope /> desalegnsefiw2@gmail.com</p>
+          <p>
+            <FaPhone />{' '}
+            <a href="tel:+251961787618" className="footer-link">
+              +251 961787618
+            </a>
+          </p>
+          <p>
+            <FaEnvelope />{' '}
+            <a href="mailto:desalegnsefiw2@gmail.com" className="footer-link">
+              desalegnsefiw2@gmail.com
+            </a>
+          </p>
         </div>
 
         <div className="social-links">
-          <a href="https://www.facebook.com/groups/163333185878581/permalink/720465120165382/?mibextid=NnVzG8" target="_blank" rel="noreferrer" aria-label="Facebook"><FaFacebook /></a>
-          <a href="https://twitter.com" target="_blank" rel="noreferrer" aria-label="Twitter"><FaTwitter /></a>
-          <a href="https://t.me/webtrainers" target="_blank" rel="noreferrer" aria-label="Telegram"><FaTelegram /></a>
+          <a
+            href="https://www.facebook.com/groups/163333185878581/permalink/720465120165382/?mibextid=NnVzG8"
+            target="_blank"
+            rel="noreferrer"
+            aria-label="Facebook"
+          >
+            <FaFacebook />
+          </a>
+          <a href="https://twitter.com" target="_blank" rel="noreferrer" aria-label="Twitter">
+            <FaTwitter />
+          </a>
+          <a href="https://t.me/webtrainers" target="_blank" rel="noreferrer" aria-label="Telegram">
+            <FaTelegram />
+          </a>
         </div>
       </div>
     </footer>
